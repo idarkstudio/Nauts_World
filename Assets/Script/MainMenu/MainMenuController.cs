2f84e75d201828b4c5215466055cfe9ce75f502d
pablo Torre <pablo.ttorre@gmail.com>
2024-02-01 18:32:05 -0300

    Temp button to change the player skin in the main menu

--- Assets/Script/MainMenu/MainMenuController.cs
+++ Assets/Script/MainMenu/MainMenuController.cs
@@ -8,19 +8,23 @@ using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-  
+    [SerializeField] private CharacterSO so;
 
 
     public void ChangeLevel(string levelName)
     {
-
         SceneManager.LoadScene(levelName);
-
 
     }
 
 
-
+    public void ChangePlayerSkin()
+    {
+        if (so.numberMat == 0)
+            so.numberMat = 1;
+        else if (so.numberMat == 1)
+            so.numberMat = 0;
+    }
 
 
 }
