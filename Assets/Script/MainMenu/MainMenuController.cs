141110362955221288422032260c49a911693934
pablo Torre <pablo.ttorre@gmail.com>
2024-06-23 04:44:11 -0300

    Conecction betwens menus

--- Assets/Script/MainMenu/MainMenuController.cs
+++ Assets/Script/MainMenu/MainMenuController.cs
@@ -13,6 +13,8 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private Sprite[] _sprites;
     [SerializeField] private Image _characterSelected;
 
+    [SerializeField] private List<CanvasGroup> menus = new List<CanvasGroup>();
+
     public void ChangeLevel(string levelName)
     {
         SceneLoadingManager.instance.ChangeScene(levelName);
@@ -23,6 +25,20 @@ public class MainMenuController : MonoBehaviour
     {
         _characterSelected.sprite = _sprites[skin];
         so.numberMat = skin;
+    }
+
+    public void ChangeMenu(CanvasGroup menuToOpen)
+    {
+        for (int i = 0; i < menus.Count; i++)
+        {
+            menus[i].alpha = 0;
+            menus[i].interactable = false;
+            menus[i].blocksRaycasts = false;
+        }
+
+        menuToOpen.alpha = 1;
+        menuToOpen.interactable = true;
+        menuToOpen.blocksRaycasts = true;
     }
 
 
