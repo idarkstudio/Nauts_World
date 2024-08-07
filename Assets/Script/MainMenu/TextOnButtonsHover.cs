7d4bfcfa27f298055e7aa2a7b334cf1af6ebc7ef
pablo Torre <pablo.ttorre@gmail.com>
2024-08-07 07:31:05 -0300

    Enabled leaderboard menu and script cleaning

--- Assets/Script/MainMenu/TextOnButtonsHover.cs
+++ Assets/Script/MainMenu/TextOnButtonsHover.cs
@@ -9,25 +9,11 @@ public class TextOnButtonsHover : MonoBehaviour
 
     public void OnMouseEnter()
     {
-        Debug.Log(666);
-        textToDisplay.SetActive(true);
-    }
-
-    private void OnMouseOver()
-    {
-        Debug.Log(555);
-        textToDisplay.SetActive(true);
-    }
-
-    private void OnMouseDown()
-    {
-        Debug.Log(444);
         textToDisplay.SetActive(true);
     }
 
     public void OnMouseExit()
     {
-        Debug.Log(777);
         textToDisplay.SetActive(false);
     }
 
